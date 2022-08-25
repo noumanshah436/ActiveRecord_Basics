0ab66b52a403f34f3b10bd9875cbf83e134d5b02
noumanshah436 <noumanshah436@gmail.com>
2022-08-25 15:25:47 +0500

    user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+
+  get 'user/index'
+  root 'product#index'
+
+  get 'product/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
