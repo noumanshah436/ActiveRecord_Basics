a2c3442ec6f885ebe59c4cab5d924d2c8bffe60e
noumanshah436 <noumanshah436@gmail.com>
2022-08-25 16:51:29 +0500

    practiced CRUD

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,14 @@
 class UserController < ApplicationController
   def index
+     user = User.create()
+  end
+
+  def my_action
+
+    # find and delete all users named David
+    User.where(name: 'Nouman').destroy_all
+
+    # delete all users
+    User.destroy_all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
   get 'user/index'
-  root 'product#index'
+  get 'user/my_action'
+  root 'user#index'
 
   get 'product/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
